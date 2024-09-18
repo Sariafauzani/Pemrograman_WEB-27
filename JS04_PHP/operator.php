8af6756df6ffe5b64548c437e1f1f88ba29c62f2
Sariafauzani <ajas2654@gmail.com>
2024-09-18 14:53:50 +0700

    modif langkah 3

--- JS04_PHP/operator.php
+++ JS04_PHP/operator.php
@@ -9,6 +9,13 @@ $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
 
+echo "Hasil Penjumlahan ($a + $b) = $hasilTambah <br>";
+echo "Hasil Pengurangan ($a - $b) = $hasilKurang <br>";
+echo "Hasil Perkalian ($a * $b) = $hasilKali <br>";
+echo "Hasil Pembagian ($a / $b) = $hasilBagi <br>";
+echo "Sisa Bagi ($a % $b) = $sisaBagi <br>";
+echo "Hasil Pangkat ($a ** $b) = $pangkat <br>";
+
 $hasilSama = $a == $b;
 $hasilTidakSama = $a != $b;
 $hasilLebihkecil = $a < $b;
