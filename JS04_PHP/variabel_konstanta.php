4b31a4e1090efa96abde50e2025df18db8fd1d6d
Sariafauzani <ajas2654@gmail.com>
2024-09-18 14:02:06 +0700

    modif-1

--- JS04_PHP/variabel_konstanta.php
+++ JS04_PHP/variabel_konstanta.php
@@ -2,16 +2,16 @@
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
-echo "Hasil pwnjumlahan $angka1 dan $angka2 adalah $hasil.";
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
 
 $benar = true;
 $salah = false;
-echo "Variabel benar: $benar, Variabel salah: $salah";
+echo "<br>Variabel benar: $benar, Variabel salah: $salah";
 
 // Mendefinisikan konstanta untuk nilai tetap
 define("NAMA_SITUS", "Websiteku.com");
 define("TAHUN_PENDIRIAN", 2023);
 
-echo "Selamat datang di " . NAMA_SITUS . ", situs yang didirikan 
+echo "<br>Selamat datang di " . NAMA_SITUS . ", situs yang didirikan 
 pada tahun " . TAHUN_PENDIRIAN . ".";
 ?>
