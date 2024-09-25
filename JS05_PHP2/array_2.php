45e208c39d71531bc305abd17ac40d42315c054d
Sariafauzani <ajas2654@gmail.com>
2024-09-25 11:12:25 +0700

    JS05-Prak2

--- JS05_PHP2/array_2.php
+++ JS05_PHP2/array_2.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta> charset="utf-8">
-        <meta> name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
     </head>
     <body>
@@ -10,7 +10,8 @@
         $Dosen = [
             'nama' => 'Elok Nur Hamdana',
             'domisili' => 'Malang',
-            'jenis_kelamin' => 'Perempuan'];
+            'jenis_kelamin' => 'Perempuan' ];
+
         echo "Nama : {$Dosen ['nama']} <br>";
         echo "Domisili : {$Dosen ['domisili']} <br>";
         echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
