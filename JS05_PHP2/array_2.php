fc10903e7a485a0f66259062ea6111e527bd8eab
Sariafauzani <ajas2654@gmail.com>
2024-09-25 11:40:00 +0700

    JS05-Prak2(modif)

--- JS05_PHP2/array_2.php
+++ JS05_PHP2/array_2.php
@@ -4,6 +4,20 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
+        <style>
+            table {
+                width: 30%;
+                margin: 20px auto;
+                border-collapse: collapse;
+            }
+            table, th, td {
+                border: 1px solid black;
+            }
+            th, td{
+                padding: 10px;
+                text-align: left;
+            }
+        </style>
     </head>
     <body>
         <?php
@@ -12,9 +26,27 @@
             'domisili' => 'Malang',
             'jenis_kelamin' => 'Perempuan' ];
 
-        echo "Nama : {$Dosen ['nama']} <br>";
+        /*echo "Nama : {$Dosen ['nama']} <br>";
         echo "Domisili : {$Dosen ['domisili']} <br>";
-        echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
+        echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";*/
         ?>
+        <table>
+            <tr>
+                <th>Attribute</th>
+                <th>Value</th>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $Dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <td>Domisili</td>
+                <td><?php echo $Dosen['domisili']; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+            </tr>
+        </table>
     </body>
 </html>
