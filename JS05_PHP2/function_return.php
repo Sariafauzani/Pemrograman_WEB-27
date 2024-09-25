cf908f62de2c7c61dfe2dae48f032362b5bc42f0
Sariafauzani <ajas2654@gmail.com>
2024-09-25 14:20:02 +0700

    JS05-Prak4

--- JS05_PHP2/function_return.php
+++ JS05_PHP2/function_return.php
@@ -4,6 +4,16 @@ function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam. ", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-echo "Umur saya adalah ". hitungUmur(2004, 2024) ."tahun";
+    //memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2004, 2024) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+//memanggil fungsi perkenalan
+    perkenalan("Saria");
+
 ?>
