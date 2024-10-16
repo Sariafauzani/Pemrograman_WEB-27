3b082e393959157e6a50faa16129d9bc7b8c1353
Sariafauzani <ajas2654@gmail.com>
2024-10-16 11:37:17 +0700

    JS08_PHP Form Upload Praktikum 1

--- JS08_PHPFormUpload/upload.php
+++ JS08_PHPFormUpload/upload.php
@@ -4,13 +4,16 @@ if(isset($_POST["submit"])) {
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    //$allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
     $maxsize = 5*1024*1024;
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
 
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah.";
+            echo "File berhasil diunggah.<br>";
+            // Untuk menampilkan gambar berhasil diunggah
+            //echo "<img src='$targetfile' style='width:200px; height:auto;' alt='Thumbnail'>";
         } else {
             echo "Gagal mengunggah file.";
         }
