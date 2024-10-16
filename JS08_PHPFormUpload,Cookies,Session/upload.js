9a5c505d3d3776072a7019404ca0b175dbed9c72
Sariafauzani <ajas2654@gmail.com>
2024-10-16 18:21:39 +0700

    JS08_Praktikum 4

--- JS08_PHPFormUpload,Cookies,Session/upload.js
+++ JS08_PHPFormUpload,Cookies,Session/upload.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+    $('#file').change(function(){
+        if (this.files.length > 0){
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
+
     $('#upload-form').submit(function(e){
         e.preventDefault();
-
+        console.log('Form submit diklik');
         var formData = new FormData(this);
-
-        $.ajax({
+         $.ajax({
             type: 'POST',
             url: 'upload_ajax.php',
             data: formData,
@@ -12,9 +19,11 @@ $(document).ready(function(){
             contentType: false,
             processData: false,
             success: function(response){
+                console.log('Respons Sukses: ', response);
                 $('#status').html(response);
             },
             error: function(){
+                console.log('Error: ' + error);
                 $('#status').html('Terjadi kesalahan saar mengunggah file.');
             }
         });
