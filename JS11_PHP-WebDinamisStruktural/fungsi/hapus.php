75010fd9eff48560546bc8ccca831583a88c1496
Sariafauzani <ajas2654@gmail.com>
2024-11-06 23:12:27 +0700

    JS11_Prak 5 Folder Module – Bagian Anggota

--- JS11_PHP-WebDinamisStruktural/fungsi/hapus.php
+++ JS11_PHP-WebDinamisStruktural/fungsi/hapus.php
@@ -1,49 +1,31 @@
 <?php
 session_start();
-if (!empty($_SESSION['username'])) {
-    require '../config/koneksi.php';
-    require '../fungsi/pesan_kilat.php';
-    require '../fungsi/anti_injection.php';
 
 if (!empty($_SESSION['username'])) {
     require '../config/koneksi.php';
-    require '../fungsi/pesan_kilat.php';
     require '../fungsi/anti_injection.php';
+    require '../fungsi/pesan_kilat.php';
+
     if (!empty($_GET['jabatan'])) {
         $id = antiinjection($koneksi, $_GET['id']);
-
-        // Menghapus anggota yang terkait dengan jabatan
-        $query2 = "DELETE FROM anggota WHERE jabatan_id = '$id'";
-        mysqli_query($koneksi, $query2);  // Menghapus anggota yang menggunakan jabatan tersebut
-
-        // Menghapus jabatan setelah anggota dihapus
         $query = "DELETE FROM jabatan WHERE id = '$id'";
         if (mysqli_query($koneksi, $query)) {
-            pesan('success', "Jabatan Telah Terhapus");
+            pesan('success', 'Jabatan berhasil dihapus');
         } else {
-            pesan('danger', "Jabatan Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            pesan('danger', 'Jabatan gagal dihapus karena:' . mysqli_error($koneksi));
         }
         header("Location: ../index.php?page=jabatan");
-    }
-}
- elseif (!empty($_GET['anggota'])) {
+    } else if (!empty($_GET['anggota'])) {
         $id = antiinjection($koneksi, $_GET['id']);
-
-        // Hapus data di tabel anggota terlebih dahulu
-        $query2 = "DELETE FROM anggota WHERE user_id = '$id'";
-        if (mysqli_query($koneksi, $query2)) {
-
-            // Jika penghapusan dari tabel anggota berhasil, lanjutkan ke penghapusan dari tabel user
-            $query = "DELETE FROM user WHERE id = '$id'";
-            if (mysqli_query($koneksi, $query)) {
-                pesan('success', "Anggota Telah Terhapus.");
+        $query = "DELETE FROM user WHERE id = '$id'";
+        if (mysqli_query($koneksi, $query)) {
+            $query2 = "DELETE FROM user WHERE id = '$id'";
+            if (mysqli_query($koneksi, $query2)) {
+                pesan('success', 'Anggota berhasil dihapus');
             } else {
-                pesan('warning', "Data Anggota Terhapus Tetapi Data Login Tidak Terhapus Karena: " . mysqli_error($koneksi));
+                pesan('danger', 'Anggota gagal dihapus karena:' . mysqli_error($koneksi));
             }
-        } else {
-            pesan('danger', "Gagal Menghapus Data Anggota Karena: " . mysqli_error($koneksi));
         }
-
         header("Location: ../index.php?page=anggota");
     }
 }
